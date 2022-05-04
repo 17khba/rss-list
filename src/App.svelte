8f00d5bedf73556ad161d666b5971efc5d8905c2
ray1214 <635794628@qq.com>
2022-05-04 15:46:16 +0800

    init

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import "rss-list";
+  import rssListData from "./data/rss-list.json";
+</script>
+
+<main>
+  <rss-list data={rssListData}>
+    <h1>Rss List</h1>
+  </rss-list>
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  }
+
+  main {
+    width: 800px;
+    text-align: left;
+    padding: 1em;
+    margin: 0 auto;
+  }
+</style>
